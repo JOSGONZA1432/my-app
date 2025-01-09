3cf7a27fae8287256f2c03c9b93d7bf0bc4cb63a
jose.ale.gt.umg <jose.ale.gt.umg@gmail.com>
2025-01-09 22:15:33 +0000

    3res

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import login from './login.jsx'
+import Login from './login.jsx'
 import './App.css'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   return (
     <div classNamd='App'>
       hola 
-<login />
+      <Login />
     </div>
   )
 }
